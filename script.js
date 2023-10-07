72006e69ff8bb98128dd534441e09cc6e71379e9
ah-an-123 <117673551+ah-an-123@users.noreply.github.com>
2023-10-07 21:09:48 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+let startButton = document.getElementById("startButton")
+let stopButton = document.getElementById("stopButton")
+let resetButton = document.getElementById("resetButton")
+let timerDisplay = document.querySelector(".timerDisplay")
+let timerId = null;
+let s = 0o0;
+let min = 0o0;
+let ms = 0o0;
+
+
+// Start Button Event Handling
+
+
+startButton.addEventListener('click', () => {
+
+    timerId = setInterval(showTime, 10)
+
+})
+
+// Stop Button Even Handler
+
+stopButton.addEventListener('click', () => {
+    // console.log("Hello Ahsan")
+    clearInterval(timerId);
+})
+
+
+// Event Handler For Reset Button
+
+resetButton.addEventListener('click', () => {
+    clearInterval(timerId);
+    timerDisplay.innerHTML = "00:00:00"
+    ms = s = min = 0;
+})
+
+// Function
+function showTime() {
+
+    if (timerId != 0) {
+        ms++;
+        if (ms == 100) {
+            s++;
+            ms = 0;
+            if (s == 60) {
+                min++;
+            }
+
+        }
+        let msString = ms < 10 ? `0${ms}` : `${ms}`
+        let sString = s < 10 ? `0${s}` : `${s}`
+        let minString = min < 10 ? `0${min}` : `${min}`
+        timerDisplay.innerHTML = `${minString}:${sString}:${msString}`
+    }
+
+
+
+}
